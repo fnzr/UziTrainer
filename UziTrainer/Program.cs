1b1ec7d2d19d9c938a64fbe7e2d9999e5a0a69a5
fnzr <5471818+fnzr@users.noreply.github.com>
2019-01-22 17:51:56 -0200

    Working out base stuff

--- UziTrainer/Program.cs
+++ UziTrainer/Program.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,16 +18,142 @@ namespace UziTrainer
             return (x & 0xFFFF) | (y & 0xFFFF) << 16;
         }
 
+        static int CompareColours(Color x, Color y)
+        {
+            return (int)(Math.Pow((int)x.R - y.R, 2) + Math.Pow((int)x.B - y.B, 2) + Math.Pow((int)x.G - y.G, 2));
+        }
+
+        public static Point FindBitmapsEntry(Bitmap sourceBitmap, Bitmap serchingBitmap)
+        {
+            #region Arguments check
+
+            if (sourceBitmap == null || serchingBitmap == null)
+                throw new ArgumentNullException();
+            Console.WriteLine(sourceBitmap.PixelFormat);
+            Console.WriteLine(serchingBitmap.PixelFormat);
+            if (sourceBitmap.PixelFormat != serchingBitmap.PixelFormat)
+                throw new ArgumentException("Pixel formats arn't equal");
+
+            if (sourceBitmap.Width < serchingBitmap.Width || sourceBitmap.Height < serchingBitmap.Height)
+                throw new ArgumentException("Size of serchingBitmap bigger then sourceBitmap");
+
+            #endregion
+
+            var pixelFormatSize = Image.GetPixelFormatSize(sourceBitmap.PixelFormat) / 8;
+
+
+            // Copy sourceBitmap to byte array
+            var sourceBitmapData = sourceBitmap.LockBits(new Rectangle(0, 0, sourceBitmap.Width, sourceBitmap.Height),
+                ImageLockMode.ReadOnly, sourceBitmap.PixelFormat);
+            var sourceBitmapBytesLength = sourceBitmapData.Stride * sourceBitmap.Height;
+            var sourceBytes = new byte[sourceBitmapBytesLength];
+            Marshal.Copy(sourceBitmapData.Scan0, sourceBytes, 0, sourceBitmapBytesLength);
+            sourceBitmap.UnlockBits(sourceBitmapData);
+
+            // Copy serchingBitmap to byte array
+            var serchingBitmapData =
+                serchingBitmap.LockBits(new Rectangle(0, 0, serchingBitmap.Width, serchingBitmap.Height),
+                    ImageLockMode.ReadOnly, serchingBitmap.PixelFormat);
+            var serchingBitmapBytesLength = serchingBitmapData.Stride * serchingBitmap.Height;
+            var serchingBytes = new byte[serchingBitmapBytesLength];
+            Marshal.Copy(serchingBitmapData.Scan0, serchingBytes, 0, serchingBitmapBytesLength);
+            serchingBitmap.UnlockBits(serchingBitmapData);
+
+            var pointsList = new List<Point>();
+
+            // Serching entries
+            // minimazing serching zone
+            // sourceBitmap.Height - serchingBitmap.Height + 1
+            for (var mainY = 0; mainY < sourceBitmap.Height - serchingBitmap.Height + 1; mainY++)
+            {
+                var sourceY = mainY * sourceBitmapData.Stride;
+
+                for (var mainX = 0; mainX < sourceBitmap.Width - serchingBitmap.Width + 1; mainX++)
+                {// mainY & mainX - pixel coordinates of sourceBitmap
+                 // sourceY + sourceX = pointer in array sourceBitmap bytes
+                    var sourceX = mainX * pixelFormatSize;
+
+                    var isEqual = true;
+                    for (var c = 0; c < pixelFormatSize; c++)
+                    {// through the bytes in pixel
+                        if (sourceBytes[sourceX + sourceY + c] == serchingBytes[c])
+                            continue;
+                        isEqual = false;
+                        break;
+                    }
+
+                    if (!isEqual) continue;
+
+                    var isStop = false;
+
+                    // find fist equalation and now we go deeper) 
+                    for (var secY = 0; secY < serchingBitmap.Height; secY++)
+                    {
+                        var serchY = secY * serchingBitmapData.Stride;
+
+                        var sourceSecY = (mainY + secY) * sourceBitmapData.Stride;
+
+                        for (var secX = 0; secX < serchingBitmap.Width; secX++)
+                        {// secX & secY - coordinates of serchingBitmap
+                         // serchX + serchY = pointer in array serchingBitmap bytes
+
+                            var serchX = secX * pixelFormatSize;
+
+                            var sourceSecX = (mainX + secX) * pixelFormatSize;
+
+                            for (var c = 0; c < pixelFormatSize; c++)
+                            {// through the bytes in pixel
+                                if (sourceBytes[sourceSecX + sourceSecY + c] == serchingBytes[serchX + serchY + c]) continue;
+
+                                // not equal - abort iteration
+                                isStop = true;
+                                break;
+                            }
+
+                            if (isStop) break;
+                        }
+
+                        if (isStop) break;
+                    }
+
+                    if (!isStop)
+                    {// serching bitmap is founded!!
+                        return  new Point(mainX, mainY);
+                    }
+                }
+            }
+            throw new InvalidDataException("[{}] not found!");
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            var hwndNox = Win32.FindWindow("Qt5QWindowIcon", "NoxPlayer");
+            var hwnd = Win32.FindWindowEx(hwndNox, 0, "Qt5QWindowIcon", "NoxPlayer");
+            var hwndPtr = new IntPtr(hwndNox);
+            RECT rc;
+            Win32.GetWindowRect(hwndPtr, out rc);
+
+            Bitmap bmp = new Bitmap(rc.Width, rc.Height, PixelFormat.Format32bppArgb);
+            Graphics gfxBmp = Graphics.FromImage(bmp);
+            IntPtr hdcBitmap = gfxBmp.GetHdc();
+
+            Win32.PrintWindow(hwndPtr, hdcBitmap, 0);
+
+            gfxBmp.ReleaseHdc(hdcBitmap);
+            gfxBmp.Dispose();
+
+            var source = new Bitmap(@"C:\Users\master\Nox_share\ImageShare\Screenshot_2019-01-22-16-39-58.png");
+            var search = new Bitmap(@"C:\Users\master\Pictures\Screenshot_3.png");
+            Console.WriteLine(FindBitmapsEntry(bmp, search));
+            return;
             Stopwatch sw = Stopwatch.StartNew();
-            for(var i=0; i<60; i++)
+            for(var i=0; i<0; i++)
             {
-                
+                /*
                 var hwndNox = Win32.FindWindow("Qt5QWindowIcon", "NoxPlayer");
                 var hwnd = Win32.FindWindowEx(hwndNox, 0, "Qt5QWindowIcon", "NoxPlayer");
                 var hwndPtr = new IntPtr(hwndNox);
@@ -41,6 +168,7 @@ namespace UziTrainer
 
                 gfxBmp.ReleaseHdc(hdcBitmap);
                 gfxBmp.Dispose();
+                */
                 /*
                 using(FileStream fs = new FileStream(Path.GetTempFileName(), FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None, 4096, FileOptions.RandomAccess | FileOptions.DeleteOnClose))
                 {                    
---
+++ UziTrainer/Window.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UziTrainer
+{
+    class Window
+    {
+        int windowHWND;
+        int messageHWND;
+
+        public Window(
+            String screenWindowClass,
+            String screenWindowTitle,            
+            String messageWindowClass,
+            String messageWindowTitle)
+        {
+            windowHWND = Win32.FindWindow(screenWindowClass, screenWindowTitle);
+            if (windowHWND <= 0) {
+                throw new ArgumentException(String.Format("No HWND for window [{0}]", screenWindowTitle));
+            }
+            messageHWND = Win32.FindWindowEx(windowHWND, 0, messageWindowClass, messageWindowTitle);
+        }
+    }
+}
