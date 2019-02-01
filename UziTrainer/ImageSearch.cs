7b8844df56a717e7529d5211b6e21627628c8507
fnzr <5471818+fnzr@users.noreply.github.com>
2019-02-01 11:18:51 -0200

    Remove outdated image search algorithm

--- UziTrainer/ImageSearch.cs
+++ UziTrainer/ImageSearch.cs
@@ -12,17 +12,6 @@ namespace UziTrainer
 {
     public class ImageSearch
     {
-        private const int DEFAULT_TOLERANCE = 20;
-        private readonly LockedBitmap haystack;
-        private readonly LockedBitmap needle;
-        private readonly int pixelFormatSize;
-
-        private ImageSearch(Bitmap _needle, Bitmap _haystack)
-        {
-            haystack = new LockedBitmap(_haystack);
-            needle = new LockedBitmap(_needle);
-            pixelFormatSize = Image.GetPixelFormatSize(_needle.PixelFormat) / 8;
-        }
 
         public static bool FindPoint(Query query, out Point coordinates)
         {                   
@@ -72,86 +61,6 @@ namespace UziTrainer
             }
             coordinates = Point.Empty;
             return false;
-        }
-
-        private static bool _Search(Bitmap bmpFind, Bitmap bmpSource, int tolerance, out Point coordinates)
-        {
-            var search = new ImageSearch(bmpFind, bmpSource);
-            var haystack = search.haystack;
-            var needle = search.needle;
-
-            for (var mainY = 0; mainY < bmpSource.Height - bmpFind.Height + 1; mainY++)
-            {
-                var sourceY = mainY * haystack.data.Stride;
-                for (var mainX = 0; mainX < bmpSource.Width - bmpFind.Width + 1; mainX++)
-                {
-                    var sourceX = mainX * search.pixelFormatSize;
-                    if (search.RegionSearch(sourceX, sourceY, tolerance))
-                    {
-                        coordinates = new Point(mainX, mainY);
-                        return true;
-                    }
-                }
-            }
-            coordinates = Point.Empty;
-            return false;
-        }
-
-        private bool RegionSearch(int startX, int startY, int tolerance)
-        {
-            for (var y = 0; y < needle.bitmap.Height; y++)
-            {
-                var needleIndexY = y * needle.data.Stride;
-                var haystackIndexY = startY + (y * haystack.data.Stride);
-                for (var x = 0; x < needle.bitmap.Width; x++)
-                {
-                    var needleIndexX = x * pixelFormatSize;
-                    var haystackIndexX = needleIndexX + startX;
-
-                    var haystackIndex = haystackIndexX + haystackIndexY;
-                    var needleIndex = needleIndexX + needleIndexY;
-                    if (!IsSameColor(haystackIndex, needleIndex, tolerance))
-                    {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
-
-        private bool IsSameColor(int haystackIndex, int needleIndex, int tolerance)
-        {
-            for (int i = 0; i < pixelFormatSize; i++)
-            {
-                if (Math.Abs(haystack.bytes[haystackIndex + i] - needle.bytes[needleIndex + i]) > tolerance)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        public void Dispose()
-        {
-
-        }
-
-        private class LockedBitmap
-        {
-            public readonly Bitmap bitmap;
-            public readonly BitmapData data;
-            public readonly byte[] bytes;
-
-            public LockedBitmap(Bitmap bitmap)
-            {
-                this.bitmap = bitmap;
-                data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
-                ImageLockMode.ReadOnly, bitmap.PixelFormat);
-                var sourceBitmapBytesLength = data.Stride * bitmap.Height;
-                bytes = new byte[sourceBitmapBytesLength];
-                Marshal.Copy(data.Scan0, bytes, 0, sourceBitmapBytesLength);
-                bitmap.UnlockBits(data);
-            }
         }
     }
 }
